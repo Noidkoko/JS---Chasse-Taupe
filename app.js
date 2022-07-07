9340ec39b74bdcfcf866ca470f1ed35d9a7b4d53
Noidkoko <108472156+Noidkoko@users.noreply.github.com>
2022-07-07 22:39:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// new Vue({
+//     el: '#app',
+//     data: {
+//         taupe: `<div class="taupe-item"><img class="taupe" src="taupe.png"></div>`
+//     }, 
+//     methods: {
+//         createBulle: function() {
+//             ret
+//         }
+//     }
+// })
